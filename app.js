8f9924b2d8d81ff38f9641f8e51276c0aaf096ad
Marco <marcomussa567@gmail.com>
2024-10-30 18:20:31 -0300

    resurection

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const bodyParser = require("body-parser")
 
 app.set("view engine", "ejs")
 
 app.set("views", __dirname + "/views")
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
+
+function isValidEmail(email) {
+    return true
+}
+
+app.post("/validar", (req, res) => {
+    const errors = {}
+    let { nombre, correo, mensaje } = req.body
+
+    if (!nombre) {
+        errors.nombre = 'El nombre es requerido'
+    }
+
+    if (!correo) {
+        errors.correo = 'El correo electrónico es requerido'
+    } else if (!isValidEmail(correo)) {
+        errors.correo = 'El correo electrónico no es válido'
+    }
+
+    if (!mensaje) {
+        errors.mensaje = 'El mensaje es requerido'
+    }
+
+    if (Object.keys(errors).length > 0) {
+        return res.render('contacto', { errors })
+    }
+
+    res.redirect("/contacto")
+})
 
 app.get("/", (req, res) => {
     res.render("index")
@@ -28,9 +58,9 @@ app.get("/servicios", (req, res) => {
     res.render("expertise")
 })
 
-app.get("/marketing", (req, res) => {
-    res.render("digital-marketing")
-})
+// app.get("/marketing", (req, res) => {
+//     res.render("digital-marketing")
+// })
 
 app.get("/desarrollo", (req, res) => {
     res.render("web-development")
@@ -39,6 +69,10 @@ app.get("/desarrollo", (req, res) => {
 app.get("/soluciones", (req, res) => {
     res.render("solutions")
 })
+
+app.use((req, res) => {
+    res.status(404).render("404");
+});
 
 app.listen(3000, () => {
     console.log("Server Started")
