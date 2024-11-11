824c8dc1dfe50ccf6c93809249987fa19bda7912
Marco <marcomussa567@gmail.com>
2024-11-11 14:22:39 -0300

    fixes, ui improvments

--- app.js
+++ app.js
@@ -13,66 +13,66 @@ function isValidEmail(email) {
     return true
 }
 
-app.post("/validar", (req, res) => {
-    const errors = {}
-    let { nombre, correo, mensaje } = req.body
+// app.post("/validar", (req, res) => {
+//     const errors = {}
+//     let { nombre, correo, mensaje } = req.body
 
-    if (!nombre) {
-        errors.nombre = 'El nombre es requerido'
-    }
+//     if (!nombre) {
+//         errors.nombre = 'El nombre es requerido'
+//     }
 
-    if (!correo) {
-        errors.correo = 'El correo electrónico es requerido'
-    } else if (!isValidEmail(correo)) {
-        errors.correo = 'El correo electrónico no es válido'
-    }
+//     if (!correo) {
+//         errors.correo = 'El correo electrónico es requerido'
+//     } else if (!isValidEmail(correo)) {
+//         errors.correo = 'El correo electrónico no es válido'
+//     }
 
-    if (!mensaje) {
-        errors.mensaje = 'El mensaje es requerido'
-    }
+//     if (!mensaje) {
+//         errors.mensaje = 'El mensaje es requerido'
+//     }
 
-    if (Object.keys(errors).length > 0) {
-        return res.render('contacto', { errors })
-    }
+//     if (Object.keys(errors).length > 0) {
+//         return res.render('contacto', { errors })
+//     }
 
-    res.redirect("/contacto")
-})
+//     res.redirect("/contacto")
+// })
 
 app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.get("/acerca", (req, res) => {
-    res.render("about")
-})
+// app.get("/acerca", (req, res) => {
+//     res.render("about")
+// })
 
-app.get("/contacto", (req, res) => {
-    res.render("contact")
-})
+// app.get("/contacto", (req, res) => {
+//     res.render("contact")
+// })
 
-app.get("/blog", (req, res) => {
-    res.render("blog")
-})
+// app.get("/blog", (req, res) => {
+//     res.render("blog")
+// })
 
-app.get("/servicios", (req, res) => {
-    res.render("expertise")
-})
+// app.get("/servicios", (req, res) => {
+//     res.render("expertise")
+// })
 
 // app.get("/marketing", (req, res) => {
 //     res.render("digital-marketing")
 // })
 
-app.get("/desarrollo", (req, res) => {
-    res.render("web-development")
-})
+// app.get("/desarrollo", (req, res) => {
+//     res.render("web-development")
+// })
 
-app.get("/soluciones", (req, res) => {
-    res.render("solutions")
-})
+// app.get("/soluciones", (req, res) => {
+//     res.render("solutions")
+// })
 
-app.use((req, res) => {
-    res.status(404).render("404");
-});
+// app.use((req, res) => {
+//     res.status(404).render("404");
+// });
 
 app.listen(3000, () => {
     console.log("Server Started")
